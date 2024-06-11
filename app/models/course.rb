f751d113ea8e959afd40c243fb9dde445c632f00
therealPatrick <malamashangala13@gmail.com>
2024-06-11 16:44:18 +0200

    model schema issue fixed

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,5 +2,5 @@ class Course < ApplicationRecord
 	has_one_attached :image do |attachable|
 		attachable.variant :thumb, resize_to_limit: [100,100]
 	end
-	has_many: lessons
+	has_many :lessons
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_08_105757) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_11_143230) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -58,6 +58,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_08_105757) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "lessons", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.boolean "paid"
+    t.integer "course_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["course_id"], name: "index_lessons_on_course_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -72,4 +82,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_08_105757) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "lessons", "courses"
 end
