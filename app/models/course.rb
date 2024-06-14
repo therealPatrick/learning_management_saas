4ded4ad009f86811df790bed35b298cdc1a6dc91
therealPatrick <malamashangala13@gmail.com>
2024-06-14 20:18:31 +0200

    video display done

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,5 @@ class Course < ApplicationRecord
 		attachable.variant :thumb, resize_to_limit: [100,100]
 	end
 	has_many :lessons
+	has_and_belongs_to_many :categories
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_14_173659) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_14_174257) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -55,6 +55,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_14_173659) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "categories_courses", force: :cascade do |t|
+    t.integer "category_id"
+    t.integer "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_categories_courses_on_category_id"
+    t.index ["course_id"], name: "index_categories_courses_on_course_id"
   end
 
   create_table "courses", force: :cascade do |t|
