f25677374c107e11c4585488eb4c1af6b1f8d614
therealPatrick <malamashangala13@gmail.com>
2024-07-02 17:00:24 +0200

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_14_174257) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_02_143425) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -71,6 +71,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_14_174257) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "paid"
+    t.string "stripe_price_id"
+    t.text "premium_description"
   end
 
   create_table "lessons", force: :cascade do |t|
